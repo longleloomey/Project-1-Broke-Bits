fe19e71e4b740fdf43cf6ba70bec2e3107a526cb
Vinhlong Le <k839w765@wichita.edu>
2018-04-03 17:26:34 -0500

    Add project files.

---
+++ CustomerClassLibrary/Customer.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomerInformationSystem
+{
+    public class Customer
+    {
+        private int customerId;
+        private string customerName;
+        private string customerEmail;
+        private string customerPhone;
+        private string customerAddress;
+
+
+        public int CustomerId
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+        public string CustomerEmail
+        {
+            get { return customerEmail; }
+            set { customerEmail = value; }
+        }
+
+
+        public string CustomerPhone
+        {
+            get { return customerPhone; }
+            set { customerPhone = value; }
+        }
+
+        public string CustomerAddress
+        {
+            get { return customerAddress; }
+            set { customerAddress = value; }
+        }
+        //default constructor
+        /// <summary>
+        /// //This method returns the customer name for provided customer Id. Will return null if not found.
+        /// <param name ="customers"></param>
+        /// <param name = "CustomerId"></param> 
+        /// </summary>
+        public Customer()
+        {
+            //do nothing
+        }
+
+        // This method return the customer name for provided customer ID> It return null if it is not found
+            
+       
+        public string GetCustomer(List<Customer> customers, int customerId)
+        {
+            foreach (Customer c in customers)
+            {
+                if(c.CustomerId == customerId)
+                {
+                    return c.CustomerName;
+                }
+
+            }
+            //If no customer found reutrn null
+            return null;
+        }
+
+        public int GetCustomer(List<Customer> customers, string customerName)
+        {
+            foreach (Customer c in customers)
+            {
+                //Convert both strings to upper case before comparing
+                if (c.CustomerName.ToUpper() == customerName.ToUpper())
+                {
+                    return c.CustomerId;
+                }
+
+            }
+            //If no customer found return 0
+            return 0;
+        }
+
+    }
+
+
+    public class Product
+    {
+        private int productId;
+        private string productName;
+        private string productCategory;
+        private string productDescription;
+        private decimal productPrice;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string ProductCategory
+        {
+            get { return productCategory; }
+            set { productCategory = value; }
+        }
+
+        public string ProductDescription
+        {
+            get { return productDescription; }
+            set { productDescription = value; }
+        }
+
+        public decimal ProductPrice
+        {
+            get { return productPrice; }
+            set { productPrice = value; }
+        }
+
+        public Product()
+        {
+            //do nothing
+        }
+
+        public string GetProduct(List<Product> products, int productId)
+        {
+            foreach (Product p in products)
+            {
+                if (p.ProductId == productId)
+                {
+                    return p.ProductName;
+                }
+
+            }
+            //If no product found return null
+            return null;
+        }
+
+        public int GetProduct(List<Product> products, string productName)
+        {
+            foreach (Product p in products)
+            {
+                //Convert both strings to upper case before comparing
+                if (p.ProductName.ToUpper() == productName.ToUpper())
+                {
+                    return p.ProductId;
+                }
+
+            }
+            //If no product found return null
+            return 0;
+        }
+    }
+
+    public class Sale
+    {
+        private int customerId;
+        private int productId;
+        private decimal purchasePrice;
+       
+
+
+        public int CustomerId
+        {
+            get { return customerId; }
+            set { customerId = value; }
+        }
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public decimal PurchasePrice
+        {
+            get { return purchasePrice; }
+            set { purchasePrice = value; }
+        }
+
+        public decimal TotalSalesByProduct (List<Sale> sales, int productId)
+        {
+            decimal totalSales = 0.0m;
+
+            foreach (Sale s in sales)
+            {
+                if(s.ProductId == productId)
+                {
+                    totalSales = totalSales + s.PurchasePrice;
+                }
+            }
+
+            return totalSales;
+        }
+
+        public decimal TotalSalesByCustomer(List<Sale> sales, int customerId)
+        {
+            decimal totalSales = 0.0m;
+
+            foreach (Sale s in sales)
+            {
+                if (s.customerId == customerId)
+                {
+                    totalSales = totalSales + s.PurchasePrice;
+                }
+            }
+
+            return totalSales;
+        }
+
+
+    }
+
+
+
+
+
+}
---
+++ CustomerProject/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using CustomerInformationSystem;
+
+namespace CustomerProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Set data for customers
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer { CustomerId=101, CustomerName="John Doe", CustomerEmail="johndoe@wichita.edu", CustomerPhone="316-123-1234", CustomerAddress="1702 Fairmount"},
+                new Customer { CustomerId=102, CustomerName="Adam Crabtree", CustomerEmail="adamcrabtree@wichita.edu", CustomerPhone="316-123-5678", CustomerAddress="1822 Fairmount"},
+                new Customer { CustomerId=103, CustomerName="Mary Jane", CustomerEmail="maryjane@wichita.edu", CustomerPhone="316-123-9090", CustomerAddress="1413 Mulberry Drive"},
+                new Customer { CustomerId=104, CustomerName="Joe Dirt", CustomerEmail="joedirt@wichita.edu", CustomerPhone="316-911-5050", CustomerAddress="542 Donut Drive"}
+
+            };
+
+            //Set data for products
+            List<Product> products = new List<Product>
+            {
+                new Product { ProductId=201, ProductName="ProductX", ProductCategory="Category1", ProductDescription="Product X dummy description", ProductPrice=19.99m},
+                new Product { ProductId=202, ProductName="ProductY", ProductCategory="Category2", ProductDescription="Product Y dummy description", ProductPrice=25.99m},
+                new Product { ProductId=202, ProductName="ProductZ", ProductCategory="Category3", ProductDescription="Product Z dummy description", ProductPrice=24.99m }
+            };
+
+            //Set data for sales
+            List<Sale> sales = new List<Sale>
+            {
+                new Sale { CustomerId=101, ProductId=201, PurchasePrice=19.99m},
+                new Sale { CustomerId=101, ProductId=202, PurchasePrice=25.99m},
+                new Sale { CustomerId=102, ProductId=201, PurchasePrice=19.99m},
+                new Sale { CustomerId=102, ProductId=202, PurchasePrice=25.99m},
+                new Sale { CustomerId=103, ProductId=201, PurchasePrice=19.99m}
+            };
+
+
+            WriteLine("Options (by letter):");
+            WriteLine("A. Search customer by ID");
+            WriteLine("B. Search customer by name");
+            WriteLine("C. Please enter an option.");
+            WriteLine("D. Search product by name.");
+            WriteLine("E. Calculate total sales by product.");
+            WriteLine("F. Calculate total sales for customer by entering name.");
+            
+            try
+            {
+                string option = Console.ReadLine();
+
+                if (option == "A" || option == "a")
+                {
+                    string customerName;
+                    WriteLine("Enter customer ID");
+                    int customerId = Convert.ToInt32(Console.ReadLine());
+
+                    Customer c = new Customer();
+
+                    customerName = c.GetCustomer(customers, customerId);
+                    WriteLine($"Customer Name: {customerName} ");
+
+
+                }
+                else if (option == "B" || option == "b")
+                {
+                    int customerId;
+                    WriteLine("Enter customer Name");
+                    string customerName = (Console.ReadLine());
+
+                    Customer c = new Customer();
+
+                    customerId = c.GetCustomer(customers, customerName);
+                    WriteLine($"Customer ID: {customerId} ");
+                }
+
+                else if (option == "C" || option == "c")
+                {
+                    string productName;
+                    WriteLine("Enter product ID");
+                    int productId = Convert.ToInt32(Console.ReadLine());
+
+                    Product p = new Product();
+
+                    productName = p.GetProduct(products, productId);
+                    WriteLine($"Product name: {productName} ");
+                }
+                else if (option == "D" || option == "d")
+                {
+                    int productId;
+                    WriteLine("Enter product Name");
+                    string productName = (Console.ReadLine());
+
+                    Product p = new Product();
+
+                    productId = p.GetProduct(products, productName);
+                    WriteLine($"Product ID: {productId} ");
+                }
+
+                else if (option == "E" || option == "e")
+                {
+                    decimal totalSales;
+                    WriteLine("Enter product ID");
+                    int pID = Convert.ToInt32(Console.ReadLine());
+
+                    Sale s = new Sale();
+
+                    totalSales = s.TotalSalesByProduct(sales, pID);
+                    WriteLine($"Sales Total: {totalSales} ");
+                }
+                else if (option == "F" || option == "f")
+                {
+                    decimal totalSales;
+                    WriteLine("Enter customer ID");
+                    int cID = Convert.ToInt32(Console.ReadLine());
+
+                    Sale s = new Sale();
+
+                    totalSales = s.TotalSalesByCustomer(sales, cID);
+                    WriteLine($"Sales Total: {totalSales} ");
+                }
+            }
+            catch (FormatException)
+            {
+                WriteLine("Invalid data type.");
+            }
+            catch (OverflowException)
+            {
+                WriteLine("Overflow Exception");
+            }
+            catch
+            {
+                WriteLine("Error: Layer 8 issue");
+            }
+        }
+
+        
+    }
+}
